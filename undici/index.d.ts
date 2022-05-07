4748df9317169c9c72813c514d21a9fe68c629a0
Masahiro Miyashiro (3846masa) <3846masahiro+git@gmail.com>
2022-05-07 22:57:00 +0900

    fix: fix type definition for undici (#106)

--- undici/index.d.ts
+++ undici/index.d.ts
@@ -1,32 +1,31 @@
+import type { CookieJar } from 'tough-cookie';
 import type { Agent, Client } from 'undici';
 
-interface CookieOptions {
+export interface CookieOptions {
   async_UNSTABLE?: true;
   jar: CookieJar;
 }
 
-namespace CookieAgent {
+export namespace CookieAgent {
   export interface Options extends Agent.Options {
     cookies?: CookieOptions | undefined;
   }
 }
 
-class CookieAgent extends Agent {
-  constructor(options: CookieAgent.Options = {});
+export class CookieAgent extends Agent {
+  constructor(options?: CookieAgent.Options);
 }
 
-namespace CookieClient {
+export namespace CookieClient {
   export interface Options extends Client.Options {
     cookies?: CookieOptions | undefined;
   }
 }
 
-class CookieClient extends Client {
+export class CookieClient extends Client {
   constructor(url: string | URL, options?: CookieClient.Options);
 }
 
-function createCookieClient<BaseClient extends Client = Client, BaseClientOptions = unknown>(
-  BaseClient: new (origin: URL, options: BaseClientOptions) => BaseClient,
-): new (origin: URL, options: BaseClientOptions & { cookies?: CookieOptions | undefined }) => CookieAgent<BaseAgent>;
-
-export { CookieAgent, CookieClient, createCookieClient };
+export function createCookieClient<BaseClient extends Client = Client, BaseClientOptions = unknown>(
+  BaseClientClass: new (origin: string | URL, options: BaseClientOptions) => BaseClient,
+): new (origin: string | URL, options?: BaseClientOptions & { cookies?: CookieOptions | undefined }) => BaseClient;
