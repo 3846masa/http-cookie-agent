d04788c70fa399d8321005571679eb36bd997a3f
3846masa <3846masahiro+git@gmail.com>
2022-06-26 15:38:58 +0900

    chore(lint): fix lint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,6 @@
 module.exports = {
   extends: [require.resolve('@3846masa/configs/eslint')],
-  overrides: [
-    {
-      files: ['examples/**/*'],
-      rules: {
-        'import/no-unresolved': [
-          'error',
-          {
-            ignore: ['http-cookie-agent'],
-          },
-        ],
-      },
-    },
-  ],
+  settings: {
+    'import/external-module-folders': ['node_modules', './http', './undici'],
+  },
 };
