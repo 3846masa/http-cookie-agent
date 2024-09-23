f8028202d472da35df9870b38492ddbd90af6788
3846masa <3846masahiro+git@gmail.com>
2024-09-23 20:14:58 +0900

    fix: prefer to use wildcard import rather than alias named import

--- http/index.d.ts
+++ http/index.d.ts
@@ -1,5 +1,5 @@
-import type { Agent as HttpAgent, AgentOptions as HttpAgentOptions } from 'node:http';
-import type { Agent as HttpsAgent, AgentOptions as HttpsAgentOptions } from 'node:https';
+import type * as http from 'node:http';
+import type * as https from 'node:https';
 
 import type { CookieJar } from 'tough-cookie';
 
@@ -11,19 +11,19 @@ export type CookieAgentOptions = {
   cookies?: CookieOptions | undefined;
 };
 
-type CookieAgent<BaseAgent extends HttpAgent> = BaseAgent;
+type CookieAgent<BaseAgent extends http.Agent> = BaseAgent;
 
-type WithCookieAgentOptions<T> = T extends HttpAgentOptions ? T & CookieAgentOptions : T;
+type WithCookieAgentOptions<T> = T extends http.AgentOptions ? T & CookieAgentOptions : T;
 type ConstructorParams<Params extends unknown[]> = {
   [Index in keyof Params]: WithCookieAgentOptions<Params[Index]>;
 };
 
-export function createCookieAgent<BaseAgent extends HttpAgent = HttpAgent, Params extends unknown[] = unknown[]>(
+export function createCookieAgent<BaseAgent extends http.Agent = http.Agent, Params extends unknown[] = unknown[]>(
   BaseAgent: new (...rest: Params) => BaseAgent,
 ): new (...rest: ConstructorParams<Params>) => CookieAgent<BaseAgent>;
 
-export const HttpCookieAgent: new (options: HttpAgentOptions & CookieAgentOptions) => CookieAgent<HttpAgent>;
-export const HttpsCookieAgent: new (options: HttpsAgentOptions & CookieAgentOptions) => CookieAgent<HttpsAgent>;
+export const HttpCookieAgent: new (options: http.AgentOptions & CookieAgentOptions) => CookieAgent<http.Agent>;
+export const HttpsCookieAgent: new (options: https.AgentOptions & CookieAgentOptions) => CookieAgent<https.Agent>;
 export const MixedCookieAgent: new (
-  options: HttpAgentOptions & HttpsAgentOptions & CookieAgentOptions,
-) => CookieAgent<HttpsAgent>;
+  options: http.AgentOptions & https.AgentOptions & CookieAgentOptions,
+) => CookieAgent<https.Agent>;
