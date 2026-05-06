dd084f1c4275e451b1bbc393c1ef01d77e2ccaab
3846masa <3846masahiro+git@gmail.com>
2026-05-07 03:11:46 +0900

    chore: remove deprecated library

--- src/types/phin.d.ts
+++
@@ -1,9 +0,0 @@
-import 'phin';
-
-declare module 'phin' {
-  namespace phin {
-    export function defaults(options: Partial<import('phin').IOptions>): typeof import('phin');
-  }
-
-  export = phin;
-}
